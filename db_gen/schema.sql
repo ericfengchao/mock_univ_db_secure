b855e7c659c672a32910e5e822c0203141d4eaf3
ericfengchao <eric.feng@thecarousell.com>
2018-11-06 17:10:15 +0800

    change schema to oracle db

--- db_gen/schema.sql
+++ db_gen/schema.sql
@@ -1,18 +1,18 @@
-DROP TABLE IF EXISTS "course_enrollment" CASCADE;
-DROP TABLE IF EXISTS "school_fee" CASCADE;
-DROP TABLE IF EXISTS "course_schedule" CASCADE;
-DROP TABLE IF EXISTS "course" CASCADE;
-DROP TABLE IF EXISTS "loan" CASCADE;
-DROP TABLE IF EXISTS "student" CASCADE;
-DROP TABLE IF EXISTS "professor" CASCADE;
-DROP TABLE IF EXISTS "admin_officer" CASCADE;
-DROP TABLE IF EXISTS "specialization" CASCADE;
-DROP TABLE IF EXISTS "semester" CASCADE;
-DROP TABLE IF EXISTS "academic_year" CASCADE;
-DROP TABLE IF EXISTS "school" CASCADE;
+DROP TABLE course_enrollment;
+DROP TABLE school_fee;
+DROP TABLE course_schedule;
+DROP TABLE course;
+DROP TABLE loan;
+DROP TABLE student;
+DROP TABLE professor;
+DROP TABLE admin_officer;
+DROP TABLE specialization;
+DROP TABLE semester;
+DROP TABLE academic_year;
+DROP TABLE school;
 
-CREATE TABLE "school" (
-    id SERIAL PRIMARY KEY,
+CREATE TABLE school (
+    id INTEGER PRIMARY KEY,
     name VARCHAR(50) NOT NULL UNIQUE,
     email VARCHAR(50) NOT NULL,
     address VARCHAR(100) NOT NULL,
@@ -20,28 +20,36 @@ CREATE TABLE "school" (
     tel VARCHAR(20) NOT NULL
 );
 
-CREATE TABLE "academic_year" (
-    id SERIAL PRIMARY KEY,
+CREATE TABLE academic_year (
+    id INTEGER PRIMARY KEY,
     start_year INTEGER NOT NULL,
     end_year INTEGER NOT NULL
 );
 
-CREATE TABLE "semester" (
-    id SERIAL PRIMARY KEY,
+CREATE TABLE semester (
+    id INTEGER PRIMARY KEY,
     semester INTEGER NOT NULL,
     start_date TIMESTAMP WITH TIME ZONE NOT NULL,
     end_date TIMESTAMP WITH TIME ZONE NOT NULL,
-    academic_year_id INTEGER REFERENCES academic_year(id) ON DELETE RESTRICT ON UPDATE CASCADE
+    academic_year_id INTEGER,
+    
+    CONSTRAINT fk_sem_academic_year 
+    FOREIGN KEY (academic_year_id)
+    REFERENCES academic_year(id)
 );
 
-CREATE TABLE "specialization" (
-    id SERIAL PRIMARY KEY,
+CREATE TABLE specialization (
+    id INTEGER PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
-    index VARCHAR(10) NOT NULL,
-    school_id INTEGER REFERENCES school(id) ON DELETE RESTRICT ON UPDATE CASCADE
+    idx VARCHAR(10) NOT NULL,
+    school_id INTEGER,
+
+    CONSTRAINT fk_spec_school
+    FOREIGN KEY (school_id)
+    REFERENCES school(id)
 );
 
-CREATE TABLE "admin_officer" (
+CREATE TABLE admin_officer (
     id VARCHAR(50) PRIMARY KEY,
     last_name VARCHAR(50) NOT NULL,
     first_name VARCHAR(50) NOT NULL,
@@ -50,10 +58,14 @@ CREATE TABLE "admin_officer" (
     office_address VARCHAR(100) NOT NULL,
     office_postal VARCHAR(10) NOT NULL,
     office_tel VARCHAR(20) NOT NULL,
-    school_id INTEGER REFERENCES school(id) ON DELETE RESTRICT ON UPDATE CASCADE
+    school_id INTEGER,
+
+   CONSTRAINT fk_admin_school
+    FOREIGN KEY (school_id)
+    REFERENCES school(id)
 );
 
-CREATE TABLE "professor" (
+CREATE TABLE professor (
     id VARCHAR(50) PRIMARY KEY,
     last_name VARCHAR(50) NOT NULL,
     first_name VARCHAR(50) NOT NULL,
@@ -62,10 +74,14 @@ CREATE TABLE "professor" (
     office_address VARCHAR(100) NOT NULL,
     office_postal VARCHAR(10) NOT NULL,
     office_tel VARCHAR(20) NOT NULL,
-    school_id INTEGER REFERENCES school(id) ON DELETE RESTRICT ON UPDATE CASCADE
+    school_id INTEGER,
+
+    CONSTRAINT fk_prof_school
+    FOREIGN KEY (school_id)
+    REFERENCES school(id)
 );
 
-CREATE TABLE "student" (
+CREATE TABLE student (
     id VARCHAR(50) PRIMARY KEY,
     last_name VARCHAR(50) NOT NULL,
     first_name VARCHAR(50) NOT NULL,
@@ -75,58 +91,87 @@ CREATE TABLE "student" (
     address VARCHAR(100) NOT NULL,
     postal VARCHAR(10) NOT NULL,
     tel VARCHAR(20) NOT NULL,
-    specialization_id INTEGER REFERENCES specialization(id) ON DELETE RESTRICT ON UPDATE CASCADE
+    specialization_id INTEGER,
+
+    CONSTRAINT fk_stu_spec
+    FOREIGN KEY(specialization_id)
+    REFERENCES specialization(id)
 );
 
-CREATE TABLE "loan" (
-    id SERIAL PRIMARY KEY,
+CREATE TABLE loan (
+    id INTEGER PRIMARY KEY,
     amount FLOAT NOT NULL,
     start_date TIMESTAMP WITH TIME ZONE NOT NULL,
     due_date TIMESTAMP WITH TIME ZONE NOT NULL,
     paid_amount FLOAT NOT NULL,
-    student_id VARCHAR(50) REFERENCES student(id) ON DELETE RESTRICT ON UPDATE CASCADE
+    student_id VARCHAR(50),
+
+    CONSTRAINT fk_loan_stu
+    FOREIGN KEY (student_id)
+    REFERENCES student(id)
 );
 
-CREATE TABLE "course" (
-    id SERIAL PRIMARY KEY,
+CREATE TABLE course (
+    id INTEGER PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
-    index INTEGER NOT NULL,
-    description TEXT NOT NULL,
-    specialization_id INTEGER REFERENCES specialization(id) ON DELETE RESTRICT ON UPDATE CASCADE
+    idx INTEGER NOT NULL,
+    description CLOB NOT NULL,
+    specialization_id INTEGER,
+
+    CONSTRAINT fk_course_spec 
+    FOREIGN KEY (specialization_id) 
+    REFERENCES specialization(id)
 );
 
-CREATE TABLE "course_schedule" (
-    id SERIAL PRIMARY KEY,
-    course_id INTEGER REFERENCES course(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    semester_id INTEGER REFERENCES semester(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    professor_id VARCHAR(50) REFERENCES professor(id) ON DELETE CASCADE ON UPDATE CASCADE
+CREATE TABLE course_schedule (
+    id INTEGER PRIMARY KEY,
+    course_id INTEGER,
+    semester_id INTEGER,
+    professor_id VARCHAR(50),
+
+    CONSTRAINT fk_cs_course
+    FOREIGN KEY (course_id)
+    REFERENCES course(id),
+
+    CONSTRAINT fk_cs_semester
+    FOREIGN KEY (semester_id)
+    REFERENCES semester(id),
+
+    CONSTRAINT fk_cs_prof
+    FOREIGN KEY (professor_id)
+    REFERENCES professor(id)
 );
 
-CREATE TABLE "school_fee" (
-    student_id VARCHAR(50) REFERENCES student(id) ON DELETE RESTRICT ON UPDATE CASCADE,
-    semester_id INTEGER REFERENCES semester(id) ON DELETE RESTRICT ON UPDATE CASCADE,
+CREATE TABLE school_fee (
+    student_id VARCHAR(50),
+    semester_id INTEGER,
     school_fee FLOAT NOT NULL,
     school_fee_paid FLOAT NOT NULL,
-    PRIMARY KEY(student_id, semester_id)
+
+    PRIMARY KEY(student_id, semester_id),
+
+    CONSTRAINT fk_sf_stu
+    FOREIGN KEY (student_id)
+    REFERENCES student(id),
+
+    CONSTRAINT fk_sf_sem
+    FOREIGN KEY (semester_id)
+    REFERENCES semester(id)
 );
 
-CREATE TABLE "course_enrollment" (
-    student_id VARCHAR(50) REFERENCES student(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    course_schedule_id INTEGER REFERENCES course_schedule(id) ON DELETE CASCADE ON UPDATE CASCADE,
+CREATE TABLE course_enrollment (
+    student_id VARCHAR(50),
+    course_schedule_id INTEGER,
     grade FLOAT NOT NULL,
-    PRIMARY KEY(student_id, course_schedule_id)
+
+    PRIMARY KEY(student_id, course_schedule_id),
+
+    CONSTRAINT fk_ce_stu
+    FOREIGN KEY (student_id)
+    REFERENCES student(id),
+
+    CONSTRAINT fk_ce_csi
+    FOREIGN KEY (course_schedule_id)
+    REFERENCES course_schedule(id)
 );
 
-
-ALTER TABLE "course_enrollment" ENABLE ROW LEVEL SECURITY;
-ALTER TABLE "school_fee" ENABLE ROW LEVEL SECURITY;
-ALTER TABLE "course_schedule" ENABLE ROW LEVEL SECURITY;
-ALTER TABLE "course" ENABLE ROW LEVEL SECURITY;
-ALTER TABLE "loan" ENABLE ROW LEVEL SECURITY;
-ALTER TABLE "student" ENABLE ROW LEVEL SECURITY;
-ALTER TABLE "professor" ENABLE ROW LEVEL SECURITY;
-ALTER TABLE "admin_officer" ENABLE ROW LEVEL SECURITY;
-ALTER TABLE "specialization" ENABLE ROW LEVEL SECURITY;
-ALTER TABLE "semester" ENABLE ROW LEVEL SECURITY;
-ALTER TABLE "academic_year" ENABLE ROW LEVEL SECURITY;
-ALTER TABLE "school" ENABLE ROW LEVEL SECURITY;
